0a4308f2bbbf9a79eab721e94e3ee3439e75f735
Peter van der Merwe <p.vandermerwe@kainos.com>
2020-05-22 05:45:37 +0100

    Fix in the release script

--- release-scripts/release.sh
+++ release-scripts/release.sh
@@ -7,9 +7,9 @@ cd ..
 ./build.sh
 
 docker tag local/mhs-inbound:latest nhsdev/nia-mhs-inbound:0.0.2
-docker tag local/mhs-outbound:latest nhsdev/nia-mhs-ioutbound:0.0.2
+docker tag local/mhs-outbound:latest nhsdev/nia-mhs-outbound:0.0.2
 docker tag local/mhs-route:latest nhsdev/nia-mhs-route:0.0.2
 
 docker push nhsdev/nia-mhs-inbound:0.0.2
-docker push nhsdev/nia-mhs-ioutbound:0.0.2
+docker push nhsdev/nia-mhs-outbound:0.0.2
 docker push nhsdev/nia-mhs-route:0.0.2
