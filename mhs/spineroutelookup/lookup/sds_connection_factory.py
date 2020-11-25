0f74ecf9b5afdae0e8a03c263753ca2a439bfa23
Bartek Sarul <b.sarul@kainos.com>
2020-11-25 15:18:52 +0100

    NIAD-714: Bump ldap lib. Change TSL protocol to 1_2 (#33)

--- mhs/spineroutelookup/lookup/sds_connection_factory.py
+++ mhs/spineroutelookup/lookup/sds_connection_factory.py
@@ -38,7 +38,7 @@ def build_sds_connection_tls(ldap_address: str, private_key: str, local_cert: st
 
     load_tls = ldap3.Tls(local_private_key_file=certificates.private_key_path,
                          local_certificate_file=certificates.local_cert_path, validate=ssl.CERT_REQUIRED,
-                         version=ssl.PROTOCOL_TLSv1, ca_certs_file=certificates.ca_certs_path)
+                         version=ssl.PROTOCOL_TLSv1_2, ca_certs_file=certificates.ca_certs_path)
 
     ldap3.set_config_parameter('RESTARTABLE_TRIES', _LDAP_CONNECTION_RETRIES)
     server = ldap3.Server(ldap_address, use_ssl=True, tls=load_tls, connect_timeout=_LDAP_CONNECTION_TIMEOUT_IN_SECONDS)
