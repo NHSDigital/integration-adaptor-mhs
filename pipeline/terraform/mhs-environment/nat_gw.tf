213326b489efe97bc9e23b5c13af9c99ef5aba15
Ashfaq Ahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-08-05 15:45:10 +0100

    Bug Fix#1

--- pipeline/terraform/mhs-environment/nat_gw.tf
+++ pipeline/terraform/mhs-environment/nat_gw.tf
@@ -1,17 +1,17 @@
-resource "aws_eip" "mhs_nat_gw_eip" {
-  vpc = true
-  tags = {
-    Name = "${var.environment_id}-mhs-nat_gw_eip"
-    EnvironmentId = var.environment_id
-  }
-}
+# resource "aws_eip" "mhs_nat_gw_eip" {
+#   vpc = true
+#   tags = {
+#     Name = "${var.environment_id}-mhs-nat_gw_eip"
+#     EnvironmentId = var.environment_id
+#   }
+# }
 
-resource "aws_nat_gateway" "mhs_nat_gw" {
-  subnet_id = aws_subnet.mhs_public_subnet.id
-  allocation_id = aws_eip.mhs_nat_gw_eip.id
+# resource "aws_nat_gateway" "mhs_nat_gw" {
+#   subnet_id = aws_subnet.mhs_public_subnet.id
+#   allocation_id = aws_eip.mhs_nat_gw_eip.id
 
-  tags = {
-    Name = "${var.environment_id}-mhs-nat_gw"
-    EnvironmentId = var.environment_id
-  }
-}
+#   tags = {
+#     Name = "${var.environment_id}-mhs-nat_gw"
+#     EnvironmentId = var.environment_id
+#   }
+# }
