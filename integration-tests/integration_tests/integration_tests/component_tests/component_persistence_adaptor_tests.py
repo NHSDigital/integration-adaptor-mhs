f390cfa5d2037e94ba8b1e7a4fed53ba93b8f7e7
Scott Alexander <scott.alexander@madetech.com>
2022-09-16 01:45:51 +0100

    import linking is broken on integration tests, creating solo use exception

--- integration-tests/integration_tests/integration_tests/component_tests/component_persistence_adaptor_tests.py
+++ integration-tests/integration_tests/integration_tests/component_tests/component_persistence_adaptor_tests.py
@@ -5,7 +5,6 @@ import unittest
 from unittest.mock import patch
 import uuid
 
-from common.exceptions import MaxRetriesExceeded
 from persistence.persistence_adaptor import PersistenceAdaptor, DuplicatePrimaryKeyError
 from persistence.persistence_adaptor_factory import get_persistence_adaptor, PERSISTENCE_ADAPTOR_TYPES
 from utilities import test_utilities
@@ -35,6 +34,8 @@ DB_KEY_FIELDS = {
     'mongodb': '_id',
 }
 
+class MaxRetriesExceeded(Exception):
+    pass
 
 class DbAdaptorsTests(unittest.TestCase):
 
