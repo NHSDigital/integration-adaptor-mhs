8846bfec55a5471ea08e3d00952b97266376e651
Steven McCullagh <steven.mccullagh@madetech.com>
2024-07-03 16:03:19 +0100

    Upgrading Python from 3.7 to 3.8 (+ license update)
    Upgrading the version of Python used from 3.7 to 3.8 and adding the year and org to the license.

--- SCR/setup.py
+++ SCR/setup.py
@@ -4,11 +4,11 @@ import setuptools
 
 setup(
     name='scr',
-    version='',
+    version='0.1',
     packages=setuptools.find_packages(),
-    url='',
-    license='',
+    url='https://digital.nhs.uk/developer/api-catalogue/summary-care-record-hl7-v3',
+    license='Apache License, Version 2.0',
     author='Phillip Woods',
-    author_email='',
+    author_email='niasupport@nhs.net',
     description='Artifacts for producing SCR requests.',
 )
--- common/setup.py
+++ common/setup.py
@@ -4,12 +4,12 @@ import setuptools
 
 setup(
     name='integration-adaptors-common',
-    version='',
+    version='0.1',
     packages=setuptools.find_packages(),
-    url='',
-    license='',
+    url='https://digital.nhs.uk/developer/api-catalogue/mhs',
+    license='Apache License, Version 2.0',
     author='Gareth Allan',
-    author_email='',
+    author_email='niasupport@nhs.net',
     description='Common utilities used by the NHS integration adaptors projects.',
     install_requires=[
         'pystache',
