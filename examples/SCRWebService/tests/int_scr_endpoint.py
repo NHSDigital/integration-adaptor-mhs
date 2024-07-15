902329064b7a1fbc7f5bdf63e3ab2a9ad6139da1
Steven McCullagh <steven.mccullagh@madetech.com>
2024-07-15 20:36:00 +0100

    One more verification

--- examples/SCRWebService/tests/int_scr_endpoint.py
+++ examples/SCRWebService/tests/int_scr_endpoint.py
@@ -1,10 +1,11 @@
-import json
 from unittest import TestCase
 from utilities import file_utilities
+import certifi
 import definitions
-import pathlib
-import os
 import handlers
+import json
+import os
+import pathlib
 
 complete_data_path = pathlib.Path(definitions.ROOT_DIR) / 'tests' / 'data' / 'complete_input.json'
 
@@ -18,5 +19,5 @@ class SCREndpointTest(TestCase):
     def test_scr_happy_path(self):
         body = file_utilities.get_file_dict(complete_data_path)
         response = handlers.post(f"{get_target_address()}/gp_summary_upload",
-                                 data=json.dumps(body))
+                                 data=json.dumps(body), verify=certifi.where())
         self.assertEqual(json.dumps(json.loads(response.text)), json.dumps(body))
