bd01def6caa848dacd9698cf87c41bd647d36f80
Steven McCullagh <steven.mccullagh@madetech.com>
2024-07-18 15:12:29 +0100

    Changes to debug and init pipeline

--- start-mhs-docker-containers.sh
+++ start-mhs-docker-containers.sh
@@ -2,8 +2,8 @@
 
 LIGHT_GREEN='\033[1;32m'
 NC='\033[0m'
+export BUILD_TAG=1.3.1
 echo -e "${LIGHT_GREEN}Stopping running containers${NC}"
 docker-compose -f docker-compose.yml stop;
-
 echo -e "${LIGHT_GREEN}Build and starting containers${NC}"
 docker-compose -f docker-compose.yml up -d --build
