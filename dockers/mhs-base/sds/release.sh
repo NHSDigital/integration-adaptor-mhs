82c1e20e921dbf43de0e2f536b599a28cc79312a
Steven McCullagh <steven.mccullagh@madetech.com>
2024-08-01 12:24:00 +0100

    Improving comments, adding test scripts v1.3.1, set MHS Inbound to not use SSL

--- dockers/mhs-base/sds/release.sh
+++ dockers/mhs-base/sds/release.sh
@@ -1,13 +1,12 @@
+# Enables 'set -ex' options for the shell to exit immediately on error and print commands as
+# they are executed. Increments the version number using the 'treeder/bump' Docker container.
+# Builds a Docker image for both ARM64 and AMD64 architectures, tags the image with 'latest'
+# and the new version number, and pushes the images to Docker Hub under the specified username.
+
 set -ex
-# SET THE FOLLOWING VARIABLES
-# docker hub username
 USERNAME=nhsdev
-# image name
 IMAGE=nia-sds-base
-# bump version
 docker run --rm -v "$PWD":/app treeder/bump patch
 version=`cat VERSION`
 echo "version: $version"
-
-# Build, tag, push
 docker buildx build -f Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag $USERNAME/$IMAGE:latest --tag $USERNAME/$IMAGE:$version --push
