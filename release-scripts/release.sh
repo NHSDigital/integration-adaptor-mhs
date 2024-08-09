9befdc3bcb9f503b0a85ce109a3487a6ca84078c
Adrian Clay <adrian@madetech.com>
2024-08-09 14:48:46 +0100

    Remove the Docker base images
    There appears to be a 1-2-1 mapping between actual image, and base
    image.
    
    This layer of indirection doesn't appear to be adding any value to
    the MHS Adaptor, and in fact makes it harder to release as there
    are two separate images for each of the inbound,outbound,sds.
    
    By combining the base image into the actual image this will
    simplify maintenance for the developers.

--- release-scripts/release.sh
+++ release-scripts/release.sh
@@ -10,7 +10,6 @@ git checkout $BUILD_TAG
 cd ..
 
 # These are buildx versions of what is inside of `build.sh`
-BASE_IMAGE_TAG="${BASE_IMAGE_TAG:-latest}"
-docker buildx build --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG  -f mhs/inbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-inbound:${BUILD_TAG} --push
-docker buildx build --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG  -f mhs/outbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-outbound:${BUILD_TAG} --push
-docker buildx build --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG  -f mhs/spineroutelookup/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-route:${BUILD_TAG} --push
+docker buildx build -f mhs/inbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-inbound:${BUILD_TAG} --push
+docker buildx build -f mhs/outbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-outbound:${BUILD_TAG} --push
+docker buildx build -f mhs/spineroutelookup/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-route:${BUILD_TAG} --push
