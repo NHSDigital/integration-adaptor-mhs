4e312334d46d9c8ff49d65e9850b149d1c2b1c1a
Adrian Clay <adrian@madetech.com>
2024-08-09 12:32:51 +0100

    Upgrade aioboto3 to version 11
    This is the latest version which supports Python 3.7
    
    Upgrading should fix bugs, and security issues.

--- mhs/common/setup.py
+++ mhs/common/setup.py
@@ -13,7 +13,7 @@ setup(
     description='Common utilities used by the NHS integration adaptors projects.',
     install_requires=[
         'defusedxml~=0.6',
-        'aioboto3~=9.6',
+        'aioboto3~=11.3',
         'tornado~=6.0',
         'isodate~=0.6',
         'marshmallow~=3.2',
