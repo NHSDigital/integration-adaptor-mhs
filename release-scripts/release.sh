79435403f5ba9eb538c09c0029217d55c40a4285
Adrian Clay <adrian@madetech.com>
2024-09-06 17:08:57 +0100

    Move adaptor Dockerfiles into the docker folder
    This consolidates Dockerfiles into one place, keeping this adaptor
    directory layout consistent with other adaptors.
    
    This also allows us to use the release-adaptor-container-image
    GitHub Actions shared workflow.

--- release-scripts/release.sh
+++ release-scripts/release.sh
@@ -10,6 +10,6 @@ git checkout $BUILD_TAG
 cd ..
 
 # These are buildx versions of what is inside of `build.sh`
-docker buildx build -f mhs/inbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-inbound:${BUILD_TAG} --push
-docker buildx build -f mhs/outbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-outbound:${BUILD_TAG} --push
-docker buildx build -f mhs/spineroutelookup/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-route:${BUILD_TAG} --push
+docker buildx build -f docker/inbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-inbound:${BUILD_TAG} --push
+docker buildx build -f docker/outbound/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-outbound:${BUILD_TAG} --push
+docker buildx build -f docker/spineroutelookup/Dockerfile . --platform linux/arm64/v8,linux/amd64 --tag nhsdev/nia-mhs-route:${BUILD_TAG} --push
